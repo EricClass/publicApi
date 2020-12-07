8a30000400b12d9c492ad291ba776d6072cb39e6
Eric TURATSINZE <turatsinzeeric43@gmail.com>
2020-12-07 21:41:56 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
+const cors = require('cors');
 
 const songsRouter = require('./routes/songsRoutes');
 const treepRouter = require('./routes/treepRoutes');
 
 const app = express();
+
+// Global Middlewares
+app.enable('trust proxy');
 app.use(express.json({ limit: '10kb' }));
+
+app.use(cors());
+
+app.options('*', cors());
 
 app.use(compression());
 
